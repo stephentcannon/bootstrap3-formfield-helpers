3bfcf06a5ca869b42967facf352c382a30767379
Steeve <stephentcannon@gmail.com>
2015-02-13 08:17:52 -0500

    latest

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ a meteor package for bootstrap 3 formfield helpers
 generates form fields
 
 {{generateTextField title='Your First Name' label='Your First Name *' id='firstName' name='firstName' placeholder='Please enter your first name.' value=firstName required='required' help='Your first name is required.'}}
+
+{{generateInputField type='number, etc...' title='Your First Name' label='Your First Name *' id='firstName' name='firstName' placeholder='Please enter your first name.' value=firstName required='required' help='Your first name is required.' min='5' max='10'}}
--- steeve:bootstrap3-formfield-helpers.js
+++ steeve:bootstrap3-formfield-helpers.js
@@ -27,4 +27,39 @@ if (Meteor.isClient) {
     return new Spacebars.SafeString(html);
     
   });
+  
+  Template.registerHelper('generateInputField', function(block){
+    var html = '<div class="form-group" data-toggle="tooltip" title="'+ block.hash.title + '"><label for="' + block.hash.name + '" class="control-label">' + block.hash.label + '</label><input type="'+ block.hash.type + '" ';
+    
+    if(block.hash.class){
+      html += ' class="form-control ' + block.hash.class + '" ';
+    } else {
+      html += ' class="form-control" ';
+    }
+    
+    html += ' id="' + block.hash.id + '" name="' + block.hash.name + '" placeholder="' + block.hash.placeholder + '" ' + block.hash.required;
+    
+    if(block.hash.min !== '' && block.hash.min !== null && block.hash.min !== undefined){
+      html += ' min="' + block.hash.min +'"';
+    }
+    
+    if(block.hash.max !== '' && block.hash.max !== null && block.hash.max !== undefined){
+      html += ' max="' + block.hash.max +'"';
+    }
+    
+    if(block.hash.value !== '' && block.hash.value !== null && block.hash.value !== undefined){
+      html += ' value="' + block.hash.value + '"';
+    }
+   
+    html += '>'
+    
+    if(block.hash.help){
+      html += '<p class="help-block">' + block.hash.help + '</p>';
+    }
+    
+    html +='</div>';
+    
+    return new Spacebars.SafeString(html);
+    
+  });
 }
