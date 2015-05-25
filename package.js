ea49188468e1bf0be5bc09354109b3fe04254c7c
Stephen Cannon <stephentcannon@gmail.com>
2015-05-25 14:52:39 -0400

    post publish

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name: 'steeve:bootstrap3-formfield-helpers',
   summary: 'a meteor package for generating bootstrap 3 form fields',
-  version: '0.0.1',
-  git: ' /* Fill me in! */ '
+  version: '0.0.2',
+  git: 'https://github.com/stephentcannon/bootstrap3-formfield-helpers.git'
 });
 
 Package.onUse(function(api) {
